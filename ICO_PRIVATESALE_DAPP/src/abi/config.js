69b8c6a29c85aa1562a9b1acbdaea655306b9892
hasansattar1 <96472041+hasansattar1@users.noreply.github.com>
2022-06-23 01:47:43 +0500

    ico dapp

---
+++ ICO_PRIVATESALE_DAPP/src/abi/config.js
@@ -0,0 +1,11 @@
+export const  ICO_CONTRACT_ADDRESS="0x615c08545cc131e7Efa6d2EDc64BF58C27e47954"
+
+export  const  TOKEN_CONTRACT="0xf5135EA29aD9705E9FFFECdBe27BFBd30a60C323"
+
+export const  TOKEN_SYMBOL="Metta"
+
+export  const  TOKEN_DECIMAL=18
+
+export  const  NETWORK_ID=56
+
+export const BLOCKCHAIN_EXPLORER= "https://bscscan.com/"
